51ad4b6b30937d9ec4a316844f8c91018dafaf59
steets@otech.nl <steets@otech.nl>
2018-10-12 10:29:42 +0200

    Added tmpl format

--- custom.el
+++ custom.el
@@ -7,6 +7,8 @@
                    (cons 'height (/ (- (x-display-pixel-height) 250) (frame-char-height)))))
   )
 
+(add-to-list 'auto-mode-alist '("\\.tmpl\\'" . jinja2-mode))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -21,7 +23,7 @@
  '(menu-bar-mode 1)
  '(package-selected-packages
    (quote
-    (helm-ag helm-descbinds helm-projectile helm counsel swiper ivy rjsx-mode jsx-mode prettier-js o-blog yaml-mode feature-mode neotree tide lua-mode markdown-mode auto-virtualenvwrapper realgud csv-mode php+-mode company-jedi jinja2-mode jedi web-mode geiser company-anaconda anaconda-mode json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window)))
+    (org-static-blog htmlize php-mode dockerfile-mode helm-ag helm-descbinds helm-projectile helm counsel swiper ivy rjsx-mode jsx-mode prettier-js o-blog yaml-mode feature-mode neotree tide lua-mode markdown-mode auto-virtualenvwrapper realgud csv-mode php+-mode company-jedi jinja2-mode jedi web-mode geiser company-anaconda anaconda-mode json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window)))
  '(prelude-auto-save nil)
  '(prelude-guru nil)
  '(realgud-safe-mode nil)
