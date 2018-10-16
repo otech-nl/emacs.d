bacb17a9e7b8195ec0a018e22f8bc4a5e04769b7
steets@otech.nl <steets@otech.nl>
2018-10-16 10:10:35 +0200

    To Linux

--- init.el
+++ init.el
@@ -12,7 +12,6 @@
    [default default default italic underline success warning error])
  '(column-number-mode t)
  '(custom-enabled-themes (quote (misterioso)))
- '(default-directory "s:/src/" t)
  '(delete-selection-mode t)
  '(display-time-24hr-format t)
  '(display-time-mode t)
@@ -46,51 +45,35 @@
 
 
 ;;; ===========================================================
-;;; packages
+;;; key bindings (C-c for user keys)
 
-;; auto-complete
-;; ac-html
-;; crux
-;; feature-mode
-;; htmlize
-;; jinja2-mode
-;; markdown-mode  ;; takes very long
-;; php-mode
-;; py-autopep8
-;; python
-;; pyvenv
-;; wc-mode
-;; web-mode
+(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+(global-set-key (kbd "C-c /") 'comment-or-uncomment-line-or-region) ;; custom function
+(global-set-key (kbd "C-c R") 'remember-region)
+(global-set-key (kbd "C-c S") 'crux-find-shell-init-file)
+(global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c d") 'crux-duplicate-current-line-or-region)
+(global-set-key (kbd "C-c e") 'crux-find-user-init-file)
+(global-set-key (kbd "C-c g") 'magit-status)
+(global-set-key (kbd "C-c j") (lambda () (interactive) (join-line -1)))  ;; join with next line
+(global-set-key (kbd "C-c l") 'package-list-packages)
+(global-set-key (kbd "C-c o") 'sort-lines)
+(global-set-key (kbd "C-c r") 'remember)
+(global-set-key (kbd "C-c s") 'crux-swap-windows)
+(global-set-key (kbd "C-c t") 'crux-visit-term-buffer)
+(global-set-key (kbd "C-c u") (lambda () (interactive) (revert-buffer-with-coding-system "unix")))
+(global-set-key (kbd "C-x k") 'kill-this-buffer)  ;; override
+(global-set-key [(control shift down)] 'move-text-down)
+(global-set-key [(control shift return)] 'crux-smart-open-line-above)
+(global-set-key [(control shift up)] 'move-text-up)
+(global-set-key [(shift return)] 'crux-smart-open-line)
+(global-set-key [remap kill-ring-save] 'easy-kill)
+(global-set-key [remap mark-sexp] 'easy-mark)
+;; (global-set-key (kbd "C-c r") 'crux-rename-buffer-and-file)
 
-(defun require-packages (packages &optional check_install)
-  "Require packages and optionally install them"
-  (dolist (p packages)
-    (message "Activating package %s" p)
-    (if (and check_install (not (package-installed-p p)))
-        (package-install p))
-    (require p)))
-
-;; Emacs packages
-(require-packages '(mouse-copy))
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
-(if (eq system-type 'windows-nt)
-    (add-to-list 'package-archives   ;; https seems broken on windows
-                 '("melpa-stable" . "http://stable.melpa.org/packages/") t)
-  (add-to-list 'package-archives
-               '("melpa-stable" . "https://stable.melpa.org/packages/") t))
-
-(require-packages package-selected-packages t)
-
-(add-to-list 'load-path "~/.emacs.d/site")
-(setq ispell-program-name "aspell" ; use aspell instead of ispell
-      ispell-extra-args '("--sug-mode=ultra"))
-;; (ac-config-default) ;; do this once
+;; (define-key key-translation-map (kbd "<s-mouse-1>") (kbd "<mouse-2>"))
+(global-set-key [remap kill-whole-line] 'crux-kill-whole-line)
 
 
 ;;; ===========================================================
@@ -113,58 +96,6 @@
   (turn-on-visual-line-mode)
   (setq-local whitespace-line-column 999))
 
-(defun duplicate-line-or-region (&optional n)
-  "Duplicate current line, or region if active.
-With argument N, make N copies.
-With negative N, comment out original line and use the absolute value."
-  (interactive)
-  (let ((use-region (use-region-p)))
-    (save-excursion
-      (let ((text (if use-region        ;Get region if active, otherwise line
-                      (buffer-substring (region-beginning) (region-end))
-                    (prog1 (thing-at-point 'line)
-                      (end-of-line)
-                      (if (< 0 (forward-line 1)) ;Go to beginning of next line, or make a new one
-                          (newline))))))
-        (dotimes (i (abs (or n 1)))     ;Insert N times, or once if not specified
-          (insert text))))
-    (if use-region nil                  ;Only if we're working with a line (not a region)
-      (let ((pos (- (point) (line-beginning-position)))) ;Save column
-        (if (> 0 n)                             ;Comment out original with negative arg
-            (comment-region (line-beginning-position) (line-end-position)))
-        (forward-line 1)
-        (forward-char pos)))))
-
-;;; ===========================================================
-;;; key bindings (C-c for user keys)
-
-(global-set-key (kbd "C-+") 'text-scale-increase)
-(global-set-key (kbd "C--") 'text-scale-decrease)
-(global-set-key (kbd "C-c /") 'comment-or-uncomment-line-or-region) ;; custom function
-(global-set-key (kbd "C-c c") 'org-capture)
-(global-set-key (kbd "C-c e") 'crux-find-user-init-file)
-(global-set-key (kbd "C-c e") 'crux-find-user-init-file)
-(global-set-key (kbd "C-c S") 'crux-find-shell-init-file)
-(global-set-key (kbd "C-c d") 'duplicate-line-or-region)
-(global-set-key (kbd "C-c j") (lambda () (interactive) (join-line -1)))  ;; join with next line
-(global-set-key (kbd "C-c l") 'package-list-packages)
-(global-set-key (kbd "C-c o") 'sort-lines)
-;; (global-set-key (kbd "C-c r") 'crux-rename-buffer-and-file)
-(global-set-key (kbd "C-c r") 'remember)
-(global-set-key (kbd "C-c R") 'remember-region)
-(global-set-key (kbd "C-c s") 'crux-swap-windows)
-(global-set-key (kbd "C-c t") 'crux-visit-term-buffer)
-(global-set-key (kbd "C-c u") (lambda () (interactive) (revert-buffer-with-coding-system "unix")))
-(global-set-key (kbd "C-x k") 'kill-this-buffer)  ;; override
-(global-set-key [(control shift down)] 'move-text-down)
-(global-set-key [(control shift return)] 'crux-smart-open-line-above)
-(global-set-key [(control shift up)] 'move-text-up)
-(global-set-key [remap kill-ring-save] 'easy-kill)
-(global-set-key [remap mark-sexp] 'easy-mark)
-(global-set-key [(shift return)] 'crux-smart-open-line)
-
-;; (define-key key-translation-map (kbd "<s-mouse-1>") (kbd "<mouse-2>"))
-(global-set-key [remap kill-whole-line] 'crux-kill-whole-line)
 
 ;;; ===========================================================
 ;;; hooks & stuff
@@ -200,7 +131,7 @@ With negative N, comment out original line and use the absolute value."
       (setq ispell-dictionary "american")
       (custom-set-variables '(default-directory "s:/src/" t))
       )
-  (custom-set-variables '(default-directory "s:/src/" t))
+  (custom-set-variables '(default-directory "~/src/" t))
   )
 
 (if window-system
@@ -213,5 +144,48 @@ With negative N, comment out original line and use the absolute value."
   )
 
 (put 'narrow-to-region 'disabled nil)
+
+;;; ===========================================================
+;;; packages
+
+;; auto-complete
+;; ac-html
+;; crux
+;; feature-mode
+;; htmlize
+;; jinja2-mode
+;; markdown-mode  ;; takes very long
+;; php-mode
+;; py-autopep8
+;; python
+;; pyvenv
+;; wc-mode
+;; web-mode
+
+;; Emacs packages
+(require 'mouse-copy)
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(if (eq system-type 'windows-nt)
+    (add-to-list 'package-archives   ;; https seems broken on windows
+                 '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+  (add-to-list 'package-archives
+               '("melpa-stable" . "https://stable.melpa.org/packages/") t))
+
+(package-refresh-contents t)
+(package-autoremove)
+(package-install-selected-packages)
+(mapcar 'require package-selected-packages)
+
+(add-to-list 'load-path "~/.emacs.d/site")
+(setq ispell-program-name "aspell" ; use aspell instead of ispell
+      ispell-extra-args '("--sug-mode=ultra"))
+;; (ac-config-default) ;; do this once
+
 
 ;;; ===========================================================
