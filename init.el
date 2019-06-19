3ef840a582cfb9833bea2e91d3956172dba360dc
steets@otech.nl <steets@otech.nl>
2019-06-19 07:25:18 +0200

    Added packages

--- init.el
+++ init.el
@@ -1,32 +1,4 @@
-;;; ===========================================================
-;;; packages
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
-(if (eq system-type 'windows-nt)
-    (add-to-list 'package-archives   ;; https seems broken on windows
-                 '("melpa-stable" . "http://stable.melpa.org/packages/") t)
-  (add-to-list 'package-archives
-               '("melpa-stable" . "https://stable.melpa.org/packages/") t))
-
-;; (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
-;; (unless ;; (require 'el-get nil 'noerror)
-;;   (with-current-buffer
-;;       (url-retrieve-synchronously
-;;        "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-;;     (goto-char (point-max))
-;;     (eval-print-last-sexp)))
-
-;; (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-;; (el-get 'sync)
-
-;;; ===========================================================
-;;; packages
+;; .emacs.d/init.el
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -43,15 +15,18 @@
  '(electric-pair-mode t)
  '(global-auto-revert-mode t)
  '(global-display-line-numbers-mode t)
- ;; '(global-hl-line-mode 1)
  '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
- '(initial-buffer-choice (lambda ()
-    (org-todo-list 1)
-    (delete-other-windows)
-    (get-buffer "*Org Agenda*")))
+ '(initial-buffer-choice
+   (lambda nil
+     (org-todo-list 1)
+     (delete-other-windows)
+     (get-buffer "*Org Agenda*")))
  '(kill-whole-line t)
- '(org-agenda-files (quote ("~/Dropbox/org/work.org" "~/Dropbox/org/administratie.org" "~/Dropbox/org/refile.org")))
+ '(mouse-avoidance-mode (quote animate) nil (avoid))
+ '(org-agenda-files
+   (quote
+    ("~/Dropbox/org/work.org" "~/Dropbox/org/administratie.org" "~/Dropbox/org/refile.org")))
  '(org-columns t)
  '(org-default-notes-file "~/Dropbox/org/refile.org")
  '(org-directory "~/Dropbox/org/")
@@ -59,11 +34,20 @@
  '(org-hide-leading-stars t)
  '(org-log-into-drawer t)
  '(org-pretty-entities t)
+ '(org-refile-targets
+   (quote
+    (("archive.org" :maxlevel . 1)
+     ("work.org" :maxlevel . 1)
+     ("sysop.org" :maxlevel . 1))))
  '(org-startup-folded nil)
  '(org-startup-indented t)
  '(org-startup-with-inline-images t)
- '(org-todo-keywords '((sequence "TODO(t)" "IN PROGRESS(p)" "WAIT(w)" "|" "DONE(d)" "CANCELED(c)")))
- '(package-selected-packages (quote (auto-complete magit)))
+ '(org-todo-keywords
+   (quote
+    ((sequence "TODO(t)" "WIP(p)" "WAITING(w)" "|" "DONE(d)" "CANCELED(c)"))))
+ '(package-selected-packages
+   (quote
+    (spaceline guru-mode writeroom-mode write-room draft-mode gherkin-mode company markdown-mode auto-package-update use-package magit)))
  '(require-final-newline t)
  '(ring-bell-function (quote ignore))
  '(save-place-mode t)
@@ -81,20 +65,89 @@
  ;; If there is more than one, they won't work right.
  )
 
+(cua-mode t)
+
 (display-time)
 
 (set-language-environment "UTF-8")
 
 (fset 'yes-or-no-p 'y-or-n-p)
 
-(ac-config-default)
+;;; ===========================================================
+;;; packages
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+
+;; bootstrap use-package (https://github.com/jwiegley/use-package)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
+(setq use-package-always-ensure t)
+(setq use-package-always-defer t)
+(use-package auto-package-update
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-hide-results t)
+  (auto-package-update-maybe))
+
+;; the actual packages
+
+(use-package draft-mode)
+
+(use-package expand-region
+  :bind ("C-=" . er/expand-region))
+
+(use-package guru-mode
+  :config
+    (guru-global-mode +1))
+
+(use-package magit
+  :bind (("C-x g" . magit-status)))  ;; default key binding fails...
+
+(use-package markdown-mode
+  :mode (("\\.text\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode)
+         ("\\.md\\'" . markdown-mode))
+  )
+
+(use-package org
+  ;; :ensure org-plus-contrib
+  :init
+  (add-hook 'org-mode-hook 'visual-line-mode)
+  (add-hook 'org-mode-hook 'org-indent-mode)
+  ;; (add-hook 'org-mode-hook 'flyspell-mode)
+  ;; (setq org-mobile-directory (expand-file-name "org" steets:drive-root))
+  :diminish visual-line-mode
+  :diminish org-indent-mode
+  :defer t
+  :bind (("\C-c l" . org-store-link)
+         ("\C-c a" . org-agenda)
+         ("\C-c c" . org-capture)
+         ("\C-c t" . (lambda () (interactive) (org-todo-list 1) (delete-other-windows)))
+         ;; ((kbd "C-c b") 'org-switchb)
+         ))
+
+(use-package spaceline)
+
+(use-package writeroom-mode
+  :config
+  (setq writeroom-width 120))
 
 ;;;; ===========================================================
 ;;; hooks
 
 (add-hook 'before-save-hook 'whitespace-cleanup)
 (add-hook 'text-mode-hook 'turn-on-visual-line-mode)
-;; (add-hook 'after-init-hook 'org-agenda-list)
+(add-hook 'after-init-hook 'global-company-mode)
 
 ;; ===========================================================
 ;;; key bindings (C-c for user keys)
@@ -114,32 +167,27 @@
 (global-set-key (kbd "C-c C-f")  ; find org file
                 (lambda() (interactive)
                   (find-file (read-file-name "Find file: " org-directory))))
-(global-set-key (kbd "C-c g") 'magit-status)
+(global-set-key (kbd "C-c i") (lambda () (interactive) (find-file "~/.emacs.d/init.el")))
 (global-set-key (kbd "C-c j") (lambda () (interactive) (join-line -1)))
 (global-set-key (kbd "C-c n") (lambda () (interactive) (find-file "~/src/notes/")))
 (global-set-key (kbd "C-c p") 'package-list-packages)
 (global-set-key (kbd "C-c r") (lambda () (interactive) (find-file "~/Dropbox/org/refile.org")))
 (global-set-key (kbd "C-c s") 'sort-lines)
-(global-set-key (kbd "C-c t") (lambda () (interactive) (org-todo-list 1) (delete-other-windows)))
 (global-set-key (kbd "C-c w") (lambda () (interactive) (find-file "~/Dropbox/org/work.org")))
-(global-set-key (kbd "C-x g") 'magit-status)  ;; default key binding fails...
-(global-set-key (kbd "C-x k") 'kill-this-buffer)  ;; override
-
-(global-set-key (kbd "C-c <up>")
+(global-set-key (kbd "C-c <up>")  ; move line up
                 (lambda () (interactive)
                   (transpose-lines 1) (forward-line -2) (indent-according-to-mode)))
-
-(global-set-key (kbd "C-c <down>")
+(global-set-key (kbd "C-c <down>")  ; move line down
                 (lambda () (interactive)
                   (forward-line 1) (transpose-lines 1) (forward-line -1)
                   (indent-according-to-mode)))
+
+;; overrides
+(global-set-key (kbd "C-x k") 'kill-this-buffer)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
 ;;; ===========================================================
 ;;; Org-mode
-
-(global-set-key (kbd "C-c l") 'org-store-link)
-(global-set-key (kbd "C-c a") 'org-agenda)
-(global-set-key (kbd "C-c c") 'org-capture)
-                                        ;(global-set-key (kbd "C-c b") 'org-switchb)
 
 (setq org-capture-templates
       `(("t" "todo" entry (file "~/Dropbox/org/refile.org") "* TODO %?\n%U\n%a")
